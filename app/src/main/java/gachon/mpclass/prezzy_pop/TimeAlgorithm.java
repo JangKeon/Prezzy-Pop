1c4798cbb4cf6581f920b5e2417ac6cc4d3c6b71
ashpurple <ashpurple9@gmail.com>
2021-05-14 13:38:20 +0900

    Create: TimeAlgorithm.java

---
+++ app/src/main/java/gachon/mpclass/prezzy_pop/TimeAlgorithm.java
@@ -0,0 +1,28 @@
+package gachon.mpclass.prezzy_pop;
+
+public class TimeAlgorithm {
+
+    private double sec;
+    private double penaltySec;
+
+    TimeAlgorithm(){};
+
+    TimeAlgorithm(int sec){
+        this.sec=sec;
+        algorithm();
+    }
+
+    public double getPenaltyTime(){
+        int penaltyTime=Integer.parseInt(String.valueOf(Math.round(penaltySec)));
+        return penaltyTime;
+    }
+
+    public void algorithm(){
+        this.penaltySec=(sec/2)*baseLog(sec);
+    }
+
+    private double baseLog(double x){
+        return Math.log(x+1)/Math.log(20);
+    }
+
+}
