f41b34144ae87f63a249b90209f67fc8bc6879fc
DW-k <pch145@naver.com>
2021-05-28 21:34:23 +0900

    update android studio

--- app/src/main/java/gachon/mpclass/prezzy_pop/SetBalloon.java
+++ app/src/main/java/gachon/mpclass/prezzy_pop/SetBalloon.java
@@ -3,19 +3,10 @@ package gachon.mpclass.prezzy_pop;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.Date;
-import java.util.HashMap;
 
 public class SetBalloon {
     public static void setCurrentBalloon(String child_key, BalloonStat balloonStat, boolean overwrite) {
