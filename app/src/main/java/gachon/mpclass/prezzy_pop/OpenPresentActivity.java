8b74e70d6e94832ba9e8f6621778b6cf64d22a1c
DESKTOP-KRO3HKH\chlgh <nyhj962@gmail.com>
2021-05-31 02:04:39 +0900

    open present activity 에 konfetti 효과 추가

--- app/src/main/java/gachon/mpclass/prezzy_pop/OpenPresentActivity.java
+++ app/src/main/java/gachon/mpclass/prezzy_pop/OpenPresentActivity.java
@@ -3,10 +3,15 @@ package gachon.mpclass.prezzy_pop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import nl.dionsegijn.konfetti.KonfettiView;
+import nl.dionsegijn.konfetti.models.Shape;
+import nl.dionsegijn.konfetti.models.Size;
 
 public class OpenPresentActivity extends AppCompatActivity {
     Button btn_openPresent;
@@ -23,6 +28,22 @@ public class OpenPresentActivity extends AppCompatActivity {
                 startMyActivity(MainActivity.class);
             }
         });
+
+        final KonfettiView konfettiView = findViewById(R.id.viewKonfetti);
+        konfettiView.build()
+                .addColors( Color.argb(1,41,205,255),
+                        Color.argb(1,120,255,68),
+                        Color.argb(1,168,100,253) ,
+                        Color.argb(1,255,113,141),
+                        Color.argb(1,253,255,106))
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(1000L)
+                .addShapes(Shape.Square.INSTANCE, Shape.Circle.INSTANCE)
+                .addSizes(new Size(12, 5f))
+                .setPosition(-50f, konfettiView.getWidth() + 50f, -50f, -50f)
+                .streamFor(300, 3000L);
     }
     private void startMyActivity(Class c) {
         Intent intent = new Intent(this, c);
